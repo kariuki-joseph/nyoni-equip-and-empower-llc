5bf1204bf7d942c350d3e7886f81e423e26aec3a
Kariuki Joseph <kariuki.joseph121@gmail.com>
2024-05-11 11:32:00 +0300

    create admin classes tabs + sample schema data

---
+++ src/assets/components/AdminClass.jsx
@@ -0,0 +1,32 @@
+import adminClassThumbnail from "../images/admin_class_thumbnail.png";
+import { BiEditAlt } from "react-icons/bi";
+import { FaRegFile } from "react-icons/fa";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
+export default function AdminClass({ classData, onEdit, onDraft, onDelete }) {
+  return (
+    <div className="my-4 px-4 py-2 shadow-lg rounded-lg">
+      <div className="flex justify-between items-center">
+        <img src={adminClassThumbnail} alt="Class Image" className="rounded" />
+        <div className="flex flex-col gap-y-3">
+          <h3 className="font-semibold text-black">{classData.title}</h3>
+          <p className="text-sm font-normal">{classData.description}</p>
+        </div>
+
+        <div className="flex flex-col gap-y-3">
+          <h3 className="font-semibold text-black">
+            No of People: {classData.peopleCount}
+          </h3>
+          <p className="text-sm">
+            Start Date : {classData.startDate} End Date : {classData.endDate}
+          </p>
+        </div>
+        <div className="flex gap-x-6">
+          <BiEditAlt className="cursor-pointer" size={24} />
+          <FaRegFile className="cursor-pointer" size={24} />
+          <RiDeleteBin6Line className="cursor-pointer" size={24} />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/assets/components/TabContent.jsx
@@ -0,0 +1,8 @@
+export default function TabContent({ mode }) {
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
